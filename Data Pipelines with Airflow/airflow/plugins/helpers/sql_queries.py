1f1f9be43bbfb2868e7927fa5a639be2dd9c9ae3
Cornisto <41071795+Cornisto@users.noreply.github.com>
2021-07-04 15:01:44 +0200

    Update sql_queries.py

--- Data Pipelines with Airflow/airflow/plugins/helpers/sql_queries.py
+++ Data Pipelines with Airflow/airflow/plugins/helpers/sql_queries.py
@@ -100,7 +100,6 @@ class SqlQueries:
     
     
     songplays_table_insert = ("""
-        INSERT INTO songplays
         SELECT
                 md5(events.sessionid || events.start_time) songplay_id,
                 events.start_time, 
@@ -121,26 +120,22 @@ class SqlQueries:
     """)
 
     users_table_insert = ("""
-        INSERT INTO users
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
         WHERE page='NextSong';
     """)
 
     songs_table_insert = ("""
-        INSERT INTO songs
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs;
     """)
 
     artists_table_insert = ("""
-        INSERT INTO artists
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
         FROM staging_songs;
     """)
 
     time_table_insert = ("""
-        INSERT INTO time
         SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays;
