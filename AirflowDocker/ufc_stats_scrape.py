067816158b9dc8171021ebed54b7c8d0d5cb7399
Cornisto <a.borkowski211@gmail.com>
2021-07-22 12:37:37 +0200

    Added UFC fighters and events scrape

---
+++ AirflowDocker/ufc_stats_scrape.py
@@ -0,0 +1,94 @@
+from string import ascii_lowercase
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import json
+
+
+class FighterInfoSpider(scrapy.Spider):
+    name = 'fighter_info'
+
+    def start_requests(self):
+        urls = [f'http://www.ufcstats.com/statistics/fighters?char={letter}&page=all' for letter in ascii_lowercase]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        fighter_profiles = response.css(
+            'table[class=b-statistics__table] \
+            tbody tr[class=b-statistics__table-row] \
+            td[class=b-statistics__table-col] a::attr(href)').getall()
+        for profile_url in fighter_profiles:
+            yield scrapy.Request(profile_url, callback=self.parse_fighter_info, meta={'request_url': profile_url})
+
+    def parse_fighter_info(self, response):
+        fighter_info = {}
+        request_url = response.meta.get('request_url')
+        fighter_name = response.css('h2[class=b-content__title] '
+                                    'span[class=b-content__title-highlight]::text').extract_first().strip()
+        fighter_info['fighter_name'] = fighter_name
+        fighter_info['fighter_record'] = response.css(
+            'h2[class=b-content__title] span[class=b-content__title-record]::text').extract_first().strip()
+        fighter_info['fighter_nickname'] = response.css('p[class=b-content__Nickname]::text').extract_first().strip()
+        fighter_hash = request_url[request_url.rfind('/') + 1:]
+        fighter_info['fighter_hash'] = fighter_hash
+        for stat in response.css('ul[class=b-list__box-list] li'):
+            stat_name = stat.css('i::text').extract_first().strip().replace(':', '').lower()
+            stat_values = [stat_val.strip() for stat_val in stat.css('*::text').extract() if
+                           stat_val.strip().replace(':', '').lower() != stat_name and stat_val.strip() != '']
+            stat_value = stat_values[0] if stat_values else ''
+            fighter_info[stat_name] = stat_value
+
+        with open(f'fighters_data/{fighter_name}.json', 'w+') as json_file:
+            json.dump(fighter_info, json_file)
+            json_file.close()
+
+        self.log(f'Collected data for {fighter_name}')
+
+
+class EventsInfoSpider(scrapy.Spider):
+    name = 'events_info'
+
+    def start_requests(self):
+        url = 'http://www.ufcstats.com/statistics/events/completed?page=all'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        event_urls = response.xpath('//table[@class="b-statistics__table-events"]/tbody/'
+                                    'tr[@class="b-statistics__table-row"]/td[@class="b-statistics__table-col"]/'
+                                    'i[@class="b-statistics__table-content"]/a/@href').getall()
+
+        for url in event_urls:
+            self.log(f'Processing data for event: {url}')
+            yield scrapy.Request(url, callback=self.parse_event_info, meta={'request_url': url})
+
+    def parse_event_info(self, response):
+
+        request_url = response.meta.get('request_url')
+
+        event_info = {'event_url': request_url,
+                      'event_name': response.xpath('//h2[@class="b-content__title"]/span['
+                                                   '@class="b-content__title-highlight"]/text()').extract_first().strip()
+                      }
+
+        for info in response.xpath('//ul[@class="b-list__box-list"]/li[@class="b-list__box-list-item"]'):
+            info_name = info.css('i::text').extract_first().strip().replace(':', '').lower()
+            info_values = [info_val.strip() for info_val in info.css('*::text').extract() if
+                           info_val.strip().replace(':', '').lower() != info_name and info_val.strip() != '']
+            info_value = info_values[0] if info_values else ''
+            event_info[info_name] = info_value
+
+        yield event_info
+
+
+if __name__ == "__main__":
+
+    process = CrawlerProcess(settings={
+        "FEEDS": {
+            "events_data/events.json": {"format": "json"},
+        },
+    })
+    # process.crawl(FighterInfoSpider)
+    process.crawl(EventsInfoSpider)
+    process.start()
